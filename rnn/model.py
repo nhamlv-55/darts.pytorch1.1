40909c36911e3f3d57cf66aa9cebfb3749dc5170
Nham Le <nle0@ilcompn0.ilcomp>
2020-08-13 08:16:05 -0700

    fix tabs width and add BiLSTM CRF

--- rnn/model.py
+++ rnn/model.py
@@ -5,98 +5,117 @@ import torch.nn.functional as F
 from genotypes import STEPS
 from utils import mask2d
 from utils import LockedDropout
-from utils import embedded_dropout
+from utils import embedded_dropout, argmax, prepare_sequence, log_sum_exp
+
+import torch.autograd as autograd
+import torch.optim as optim
+
+torch.manual_seed(1)
 
 INITRANGE = 0.04
 
 
 class DARTSCell(nn.Module):
+    def __init__(self, ninp, nhid, dropouth, dropoutx, genotype):
+        super(DARTSCell, self).__init__()
+        self.nhid = nhid
+        self.dropouth = dropouth
+        self.dropoutx = dropoutx
+        self.genotype = genotype
 
-  def __init__(self, ninp, nhid, dropouth, dropoutx, genotype):
-    super(DARTSCell, self).__init__()
-    self.nhid = nhid
-    self.dropouth = dropouth
-    self.dropoutx = dropoutx
-    self.genotype = genotype
+        # genotype is None when doing arch search
+        steps = len(
+            self.genotype.recurrent) if self.genotype is not None else STEPS
+        self._W0 = nn.Parameter(
+            torch.Tensor(ninp + nhid,
+                         2 * nhid).uniform_(-INITRANGE, INITRANGE))
+        self._Ws = nn.ParameterList([
+            nn.Parameter(
+                torch.Tensor(nhid, 2 * nhid).uniform_(-INITRANGE, INITRANGE))
+            for i in range(steps)
+        ])
 
-    # genotype is None when doing arch search
-    steps = len(self.genotype.recurrent) if self.genotype is not None else STEPS
-    self._W0 = nn.Parameter(torch.Tensor(ninp+nhid, 2*nhid).uniform_(-INITRANGE, INITRANGE))
-    self._Ws = nn.ParameterList([
-        nn.Parameter(torch.Tensor(nhid, 2*nhid).uniform_(-INITRANGE, INITRANGE)) for i in range(steps)
-    ])
+    def forward(self, inputs, hidden):
+        T, B = inputs.size(0), inputs.size(1)
 
-  def forward(self, inputs, hidden):
-    T, B = inputs.size(0), inputs.size(1)
+        if self.training:
+            x_mask = mask2d(B, inputs.size(2), keep_prob=1. - self.dropoutx)
+            h_mask = mask2d(B, hidden.size(2), keep_prob=1. - self.dropouth)
+        else:
+            x_mask = h_mask = None
 
-    if self.training:
-      x_mask = mask2d(B, inputs.size(2), keep_prob=1.-self.dropoutx)
-      h_mask = mask2d(B, hidden.size(2), keep_prob=1.-self.dropouth)
-    else:
-      x_mask = h_mask = None
+        hidden = hidden[0]
+        hiddens = []
+        for t in range(T):
+            hidden = self.cell(inputs[t], hidden, x_mask, h_mask)
+            hiddens.append(hidden)
+            hiddens = torch.stack(hiddens)
+        return hiddens, hiddens[-1].unsqueeze(0)
 
-    hidden = hidden[0]
-    hiddens = []
-    for t in range(T):
-      hidden = self.cell(inputs[t], hidden, x_mask, h_mask)
-      hiddens.append(hidden)
-    hiddens = torch.stack(hiddens)
-    return hiddens, hiddens[-1].unsqueeze(0)
+    def _compute_init_state(self, x, h_prev, x_mask, h_mask):
+        if self.training:
+            xh_prev = torch.cat([x * x_mask, h_prev * h_mask], dim=-1)
+        else:
+            xh_prev = torch.cat([x, h_prev], dim=-1)
+            c0, h0 = torch.split(xh_prev.mm(self._W0), self.nhid, dim=-1)
+            c0 = c0.sigmoid()
+            h0 = h0.tanh()
+            s0 = h_prev + c0 * (h0 - h_prev)
+        return s0
 
-  def _compute_init_state(self, x, h_prev, x_mask, h_mask):
-    if self.training:
-      xh_prev = torch.cat([x * x_mask, h_prev * h_mask], dim=-1)
-    else:
-      xh_prev = torch.cat([x, h_prev], dim=-1)
-    c0, h0 = torch.split(xh_prev.mm(self._W0), self.nhid, dim=-1)
-    c0 = c0.sigmoid()
-    h0 = h0.tanh()
-    s0 = h_prev + c0 * (h0-h_prev)
-    return s0
+    def _get_activation(self, name):
+        if name == 'tanh':
+            f = torch.tanh
+        elif name == 'relu':
+            f = torch.relu
+        elif name == 'sigmoid':
+            f = torch.sigmoid
+        elif name == 'identity':
+            f = lambda x: x
+        else:
+            raise NotImplementedError
+        return f
 
-  def _get_activation(self, name):
-    if name == 'tanh':
-      f = torch.tanh
-    elif name == 'relu':
-      f = torch.relu
-    elif name == 'sigmoid':
-      f = torch.sigmoid
-    elif name == 'identity':
-      f = lambda x: x
-    else:
-      raise NotImplementedError
-    return f
+    def cell(self, x, h_prev, x_mask, h_mask):
+        s0 = self._compute_init_state(x, h_prev, x_mask, h_mask)
 
-  def cell(self, x, h_prev, x_mask, h_mask):
-    s0 = self._compute_init_state(x, h_prev, x_mask, h_mask)
-
-    states = [s0]
-    for i, (name, pred) in enumerate(self.genotype.recurrent):
-      s_prev = states[pred]
-      if self.training:
-        ch = (s_prev * h_mask).mm(self._Ws[i])
-      else:
-        ch = s_prev.mm(self._Ws[i])
-      c, h = torch.split(ch, self.nhid, dim=-1)
-      c = c.sigmoid()
-      fn = self._get_activation(name)
-      h = fn(h)
-      s = s_prev + c * (h-s_prev)
-      states += [s]
-    output = torch.mean(torch.stack([states[i] for i in self.genotype.concat], -1), -1)
-    return output
+        states = [s0]
+        for i, (name, pred) in enumerate(self.genotype.recurrent):
+            s_prev = states[pred]
+            if self.training:
+                ch = (s_prev * h_mask).mm(self._Ws[i])
+            else:
+                ch = s_prev.mm(self._Ws[i])
+                c, h = torch.split(ch, self.nhid, dim=-1)
+                c = c.sigmoid()
+                fn = self._get_activation(name)
+                h = fn(h)
+                s = s_prev + c * (h - s_prev)
+                states += [s]
+                output = torch.mean(
+                    torch.stack([states[i] for i in self.genotype.concat], -1),
+                    -1)
+        return output
 
 
 class RNNModel(nn.Module):
     """Container module with an encoder, a recurrent module, and a decoder."""
-
-    def __init__(self, ntoken, ninp, nhid, nhidlast, 
-                 dropout=0.5, dropouth=0.5, dropoutx=0.5, dropouti=0.5, dropoute=0.1,
-                 cell_cls=DARTSCell, genotype=None):
+    def __init__(self,
+                 ntoken,
+                 ninp,
+                 nhid,
+                 nhidlast,
+                 dropout=0.5,
+                 dropouth=0.5,
+                 dropoutx=0.5,
+                 dropouti=0.5,
+                 dropoute=0.1,
+                 cell_cls=DARTSCell,
+                 genotype=None):
         super(RNNModel, self).__init__()
         self.lockdrop = LockedDropout()
         self.encoder = nn.Embedding(ntoken, ninp)
-        
+
         assert ninp == nhid == nhidlast
         if cell_cls == DARTSCell:
             assert genotype is not None
@@ -107,7 +126,7 @@ class RNNModel(nn.Module):
 
         self.rnns = torch.nn.ModuleList(self.rnns)
         self.decoder = nn.Linear(ninp, ntoken)
-        self.decoder.weight = self.encoder.weight # weight tying
+        self.decoder.weight = self.encoder.weight  # weight tying
         self.init_weights()
 
         self.ninp = ninp
@@ -127,7 +146,9 @@ class RNNModel(nn.Module):
     def forward(self, input, hidden, return_h=False):
         batch_size = input.size(1)
 
-        emb = embedded_dropout(self.encoder, input, dropout=self.dropoute if self.training else 0)
+        emb = embedded_dropout(self.encoder,
+                               input,
+                               dropout=self.dropoute if self.training else 0)
         emb = self.lockdrop(emb, self.dropouti)
 
         raw_output = emb
@@ -154,6 +175,204 @@ class RNNModel(nn.Module):
         return model_output, hidden
 
     def init_hidden(self, bsz):
-      weight = next(self.parameters()).data
-      return [weight.new(1, bsz, self.nhid).zero_()]
+        weight = next(self.parameters()).data
+        return [weight.new(1, bsz, self.nhid).zero_()]
 
+class BiLSTM_CRF(nn.Module):
+
+    def __init__(self, vocab_size, tag_to_ix, embedding_dim, hidden_dim):
+        super(BiLSTM_CRF, self).__init__()
+        self.embedding_dim = embedding_dim
+        self.hidden_dim = hidden_dim
+        self.vocab_size = vocab_size
+        self.tag_to_ix = tag_to_ix
+        self.tagset_size = len(tag_to_ix)
+
+        self.word_embeds = nn.Embedding(vocab_size, embedding_dim)
+        self.lstm = nn.LSTM(embedding_dim, hidden_dim // 2,
+                            num_layers=1, bidirectional=True)
+
+        # Maps the output of the LSTM into tag space.
+        self.hidden2tag = nn.Linear(hidden_dim, self.tagset_size)
+
+        # Matrix of transition parameters.  Entry i,j is the score of
+        # transitioning *to* i *from* j.
+        self.transitions = nn.Parameter(
+            torch.randn(self.tagset_size, self.tagset_size))
+
+        # These two statements enforce the constraint that we never transfer
+        # to the start tag and we never transfer from the stop tag
+        self.transitions.data[tag_to_ix[START_TAG], :] = -10000
+        self.transitions.data[:, tag_to_ix[STOP_TAG]] = -10000
+
+        self.hidden = self.init_hidden()
+
+    def init_hidden(self):
+        return (torch.randn(2, 1, self.hidden_dim // 2),
+                torch.randn(2, 1, self.hidden_dim // 2))
+
+    def _forward_alg(self, feats):
+        # Do the forward algorithm to compute the partition function
+        init_alphas = torch.full((1, self.tagset_size), -10000.)
+        # START_TAG has all of the score.
+        init_alphas[0][self.tag_to_ix[START_TAG]] = 0.
+
+        # Wrap in a variable so that we will get automatic backprop
+        forward_var = init_alphas
+
+        # Iterate through the sentence
+        for feat in feats:
+            alphas_t = []  # The forward tensors at this timestep
+            for next_tag in range(self.tagset_size):
+                # broadcast the emission score: it is the same regardless of
+                # the previous tag
+                emit_score = feat[next_tag].view(
+                    1, -1).expand(1, self.tagset_size)
+                # the ith entry of trans_score is the score of transitioning to
+                # next_tag from i
+                trans_score = self.transitions[next_tag].view(1, -1)
+                # The ith entry of next_tag_var is the value for the
+                # edge (i -> next_tag) before we do log-sum-exp
+                next_tag_var = forward_var + trans_score + emit_score
+                # The forward variable for this tag is log-sum-exp of all the
+                # scores.
+                alphas_t.append(log_sum_exp(next_tag_var).view(1))
+            forward_var = torch.cat(alphas_t).view(1, -1)
+        terminal_var = forward_var + self.transitions[self.tag_to_ix[STOP_TAG]]
+        alpha = log_sum_exp(terminal_var)
+        return alpha
+
+    def _get_lstm_features(self, sentence):
+        self.hidden = self.init_hidden()
+        embeds = self.word_embeds(sentence).view(len(sentence), 1, -1)
+        lstm_out, self.hidden = self.lstm(embeds, self.hidden)
+        lstm_out = lstm_out.view(len(sentence), self.hidden_dim)
+        lstm_feats = self.hidden2tag(lstm_out)
+        return lstm_feats
+
+    def _score_sentence(self, feats, tags):
+        # Gives the score of a provided tag sequence
+        score = torch.zeros(1)
+        tags = torch.cat([torch.tensor([self.tag_to_ix[START_TAG]], dtype=torch.long), tags])
+        for i, feat in enumerate(feats):
+            score = score + \
+                self.transitions[tags[i + 1], tags[i]] + feat[tags[i + 1]]
+        score = score + self.transitions[self.tag_to_ix[STOP_TAG], tags[-1]]
+        return score
+
+    def _viterbi_decode(self, feats):
+        backpointers = []
+
+        # Initialize the viterbi variables in log space
+        init_vvars = torch.full((1, self.tagset_size), -10000.)
+        init_vvars[0][self.tag_to_ix[START_TAG]] = 0
+
+        # forward_var at step i holds the viterbi variables for step i-1
+        forward_var = init_vvars
+        for feat in feats:
+            bptrs_t = []  # holds the backpointers for this step
+            viterbivars_t = []  # holds the viterbi variables for this step
+
+            for next_tag in range(self.tagset_size):
+                # next_tag_var[i] holds the viterbi variable for tag i at the
+                # previous step, plus the score of transitioning
+                # from tag i to next_tag.
+                # We don't include the emission scores here because the max
+                # does not depend on them (we add them in below)
+                next_tag_var = forward_var + self.transitions[next_tag]
+                best_tag_id = argmax(next_tag_var)
+                bptrs_t.append(best_tag_id)
+                viterbivars_t.append(next_tag_var[0][best_tag_id].view(1))
+            # Now add in the emission scores, and assign forward_var to the set
+            # of viterbi variables we just computed
+            forward_var = (torch.cat(viterbivars_t) + feat).view(1, -1)
+            backpointers.append(bptrs_t)
+
+        # Transition to STOP_TAG
+        terminal_var = forward_var + self.transitions[self.tag_to_ix[STOP_TAG]]
+        best_tag_id = argmax(terminal_var)
+        path_score = terminal_var[0][best_tag_id]
+
+        # Follow the back pointers to decode the best path.
+        best_path = [best_tag_id]
+        for bptrs_t in reversed(backpointers):
+            best_tag_id = bptrs_t[best_tag_id]
+            best_path.append(best_tag_id)
+        # Pop off the start tag (we dont want to return that to the caller)
+        start = best_path.pop()
+        assert start == self.tag_to_ix[START_TAG]  # Sanity check
+        best_path.reverse()
+        return path_score, best_path
+
+    def neg_log_likelihood(self, sentence, tags):
+        feats = self._get_lstm_features(sentence)
+        forward_score = self._forward_alg(feats)
+        gold_score = self._score_sentence(feats, tags)
+        return forward_score - gold_score
+
+    def forward(self, sentence):  # dont confuse this with _forward_alg above.
+        # Get the emission scores from the BiLSTM
+        lstm_feats = self._get_lstm_features(sentence)
+
+        # Find the best path, given the features.
+        score, tag_seq = self._viterbi_decode(lstm_feats)
+        return score, tag_seq
+
+if __name__=="__main__":
+    START_TAG = "<START>"
+    STOP_TAG = "<STOP>"
+    EMBEDDING_DIM = 5
+    HIDDEN_DIM = 4
+
+    # Make up some training data
+    training_data = [(
+        "the wall street journal reported today that apple corporation made money".split(),
+        "B I I I O O O B I O O".split()
+    ), (
+        "georgia tech is a university in georgia".split(),
+        "B I O O O O B".split()
+    )]
+
+    word_to_ix = {}
+    for sentence, tags in training_data:
+        for word in sentence:
+            if word not in word_to_ix:
+                word_to_ix[word] = len(word_to_ix)
+
+    tag_to_ix = {"B": 0, "I": 1, "O": 2, START_TAG: 3, STOP_TAG: 4}
+
+    model = BiLSTM_CRF(len(word_to_ix), tag_to_ix, EMBEDDING_DIM, HIDDEN_DIM)
+    optimizer = optim.SGD(model.parameters(), lr=0.01, weight_decay=1e-4)
+
+    # Check predictions before training
+    with torch.no_grad():
+        precheck_sent = prepare_sequence(training_data[0][0], word_to_ix)
+        precheck_tags = torch.tensor([tag_to_ix[t] for t in training_data[0][1]], dtype=torch.long)
+        print(model(precheck_sent))
+
+    # Make sure prepare_sequence from earlier in the LSTM section is loaded
+    for epoch in range(
+            300):  # again, normally you would NOT do 300 epochs, it is toy data
+        for sentence, tags in training_data:
+            # Step 1. Remember that Pytorch accumulates gradients.
+            # We need to clear them out before each instance
+            model.zero_grad()
+
+            # Step 2. Get our inputs ready for the network, that is,
+            # turn them into Tensors of word indices.
+            sentence_in = prepare_sequence(sentence, word_to_ix)
+            targets = torch.tensor([tag_to_ix[t] for t in tags], dtype=torch.long)
+
+            # Step 3. Run our forward pass.
+            loss = model.neg_log_likelihood(sentence_in, targets)
+
+            # Step 4. Compute the loss, gradients, and update the parameters by
+            # calling optimizer.step()
+            loss.backward()
+            optimizer.step()
+
+    # Check predictions after training
+    with torch.no_grad():
+        precheck_sent = prepare_sequence(training_data[0][0], word_to_ix)
+        print(model(precheck_sent))
+    # We got it!
